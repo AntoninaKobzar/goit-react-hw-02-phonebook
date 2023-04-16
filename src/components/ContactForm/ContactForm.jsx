aa86442b7670d9209c623fb70159f000d23535c0
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-04-16 12:39:33 +0300

    without css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,41 +1,77 @@
 import { Component } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import css from '../ContactForm/ContactForm.module.css';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import css from './ContactForm.module.css';
 
 
-class ContactForm extends Component{
-    state = { name:'',};
+class ContactForm extends Component {
+    // static propTypes = {
+    //     onSubmit: PropTypes.func.isRequired,
+    // };
+    state = {
+        name: '',
+        number: '',
+    };
     
     handleChange = event => {
         const { name, value } = event.currentTarget;
-        this.setState({ [name]: value, });
+        this.setState({
+            [name]: value,
+        });
     };
     handleSubmit = event => {
         event.preventDefault();
         const contact = {
-            id: uuidv4(),
+            id: nanoid(),
             name: this.state.name,
+            number: this.state.number,
         };
-        // this.props.onSubmit(contact);
-        // this.resetForm();
+
+        this.props.onSubmit(contact);
+        this.resetForm();
+    };
+    resetForm = () => {
+        this.setState({
+            id: '',
+            name: '',
+            number: '',
+        });
     };
 
-    render(){
-    return (
-        <form className={css.form} onSubmit={this.handleSubmit}>
-            <label>
-                <p  className={css.form__label}>Name</p>
-                <input className={css.form__input} type="text"
+    render() {
+        return (
+            <form className={css.form} onSubmit={this.handleSubmit}>
+                <label>
+                    <p className={css.form__label}>Name</p>
+                    <input
+                        className={css.form__input}
+                        type="text"
                         name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required>
-                </input>
-            </label>
-            <button type="submit" className={css.form__button}>Add contact</button>
-        </form>
-    );
-}
+                        title="Name may contain only letters, apostrophe, dash and spaces. 
+                    For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                />
+                </label>
+                <label>
+                    Number
+                    <input
+                        className={css.form__input}
+                        type="tel"
+                        name="number"
+                        value={this.state.number}
+                        onChange={this.handleChange}
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                    />
+                </label>
+                <button type="submit" className={css.form__button}>Add contact</button>
+            </form>
+        );
+    }
 }
 
 
--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,9 +1,13 @@
 import css from './ContactItem.module.css';
 
-const ContactItem = ({ contact}) => {
+const ContactItem = ({ contact,onDeleteContact}) => {
     return (
         <li>
-            <span className={css.item}>{contact.name} </span>
+            <span className={css.item}>{contact.name}: </span>
+            <a href={`tel:${contact.number}`}>{contact.number}</a>
+            <button
+                type='button'
+                onClick={()=>onDeleteContact(contact.id)}>Delete</button>
         </li>
     );
 };
